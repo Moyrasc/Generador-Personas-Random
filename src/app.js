7d5a46fd23de4964a1c6d1d3b78c704a04387a11
Moyra_sc <noemisolac@gmail.com>
2022-07-05 09:18:49 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,77 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+document.querySelector(".btn").addEventListener("click", () => {
+  document.querySelector(".subtitle").textContent = generatePersonRandom();
+});
+function generatePersonRandom() {
+  let person = [
+    {
+      name: "maria",
+      lastName: "rodriguez"
+    },
+    {
+      name: "pedro",
+      lastName: "perez"
+    },
+    {
+      name: "juan jose",
+      lastName: "ruiz"
+    }
+  ];
+  let address = [
+    {
+      cp: 2080,
+      dir: {
+        city: "madrid",
+        avenida: "alcalá"
+      }
+    },
+    {
+      cp: 2085,
+      dir: {
+        city: "barcelona",
+        avenida: "rua"
+      }
+    },
+    {
+      cp: 2087,
+      dir: {
+        city: "murcia",
+        avenida: "atocha"
+      }
+    }
+  ];
+  let job = [
+    {
+      company: "Google",
+      position: {
+        job: "fullstack developer",
+        level: "junior"
+      },
+      antiquity: 5
+    },
+    {
+      company: "Amazon",
+      position: {
+        job: "back-end developer ",
+        level: "senior"
+      },
+      antiquity: 5
+    },
+    {
+      company: "Google",
+      position: {
+        job: "front-end developer ",
+        level: "senior"
+      },
+      antiquity: 5
+    }
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let personRandom = Math.floor(Math.random() * person.length);
+  let addressRandom = Math.floor(Math.random() * address.length);
+  let jobRandom = Math.floor(Math.random() * job.length);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let elements = ` ${person[personRandom].name} ${address[addressRandom].dir.city} 
+  ${job[jobRandom].position.job}`;
+
+  return elements;
+}
